969c1cd4cc723afc4353ec2b954e99b37c64e04e
spences10 <spences10apps@gmail.com>
2019-10-22 17:16:00 +0100

    all in one file! 😱

--- index.js
+++ index.js
@@ -3,93 +3,6 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.Facebook = void 0;
-
-var _propTypes = _interopRequireDefault(require("prop-types"));
-
-var _react = _interopRequireDefault(require("react"));
-
-var _reactHelmet = _interopRequireDefault(require("react-helmet"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-var Facebook = function Facebook(_ref) {
-  var url = _ref.url,
-      type = _ref.type,
-      title = _ref.title,
-      desc = _ref.desc,
-      image = _ref.image,
-      locale = _ref.locale;
-  return _react["default"].createElement(_reactHelmet["default"], null, _react["default"].createElement("meta", {
-    property: "og:locale",
-    content: locale
-  }), _react["default"].createElement("meta", {
-    property: "og:url",
-    content: url
-  }), _react["default"].createElement("meta", {
-    property: "og:type",
-    content: type
-  }), _react["default"].createElement("meta", {
-    property: "og:title",
-    content: title
-  }), _react["default"].createElement("meta", {
-    property: "og:description",
-    content: desc
-  }), _react["default"].createElement("meta", {
-    property: "og:image",
-    content: image
-  }), _react["default"].createElement("meta", {
-    property: "og:image:alt",
-    content: desc
-  }));
-};
-
-exports.Facebook = Facebook;
-Facebook.propTypes = {
-  url: _propTypes["default"].string.isRequired,
-  locale: _propTypes["default"].string.isRequired,
-  type: _propTypes["default"].string,
-  title: _propTypes["default"].string.isRequired,
-  desc: _propTypes["default"].string.isRequired,
-  image: _propTypes["default"].string.isRequired,
-  name: _propTypes["default"].string
-};
-Facebook.defaultProps = {
-  type: 'website',
-  name: null
-};
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-Object.defineProperty(exports, "Facebook", {
-  enumerable: true,
-  get: function get() {
-    return _facebook.Facebook;
-  }
-});
-Object.defineProperty(exports, "Twitter", {
-  enumerable: true,
-  get: function get() {
-    return _twitter.Twitter;
-  }
-});
-exports["default"] = void 0;
-
-var _facebook = require("./facebook");
-
-var _seo = require("./seo");
-
-var _twitter = require("./twitter");
-
-var _default = _seo.SEO;
-exports["default"] = _default;
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
 exports.SEO = void 0;
 
 var _propTypes = _interopRequireDefault(require("prop-types"));
@@ -97,10 +10,6 @@ var _propTypes = _interopRequireDefault(require("prop-types"));
 var _react = _interopRequireDefault(require("react"));
 
 var _reactHelmet = _interopRequireDefault(require("react-helmet"));
-
-var _facebook = require("./facebook");
-
-var _twitter = require("./twitter");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
@@ -127,14 +36,14 @@ var SEO = function SEO(_ref) {
   }), _react["default"].createElement("meta", {
     name: "description",
     content: seo.description
-  })), _react["default"].createElement(_facebook.Facebook, {
+  })), _react["default"].createElement(Facebook, {
     desc: seo.description,
     image: seo.image,
     title: seo.title,
     type: article ? 'article' : 'website',
     url: seo.url,
     locale: siteLocale
-  }), _react["default"].createElement(_twitter.Twitter, {
+  }), _react["default"].createElement(Twitter, {
     title: seo.title,
     image: seo.image,
     desc: seo.description,
@@ -147,27 +56,58 @@ SEO.propTypes = {
   title: _propTypes["default"].string.isRequired,
   description: _propTypes["default"].string
 };
-"use strict";
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.Twitter = void 0;
+var Facebook = function Facebook(_ref2) {
+  var url = _ref2.url,
+      type = _ref2.type,
+      title = _ref2.title,
+      desc = _ref2.desc,
+      image = _ref2.image,
+      locale = _ref2.locale;
+  return _react["default"].createElement(_reactHelmet["default"], null, _react["default"].createElement("meta", {
+    property: "og:locale",
+    content: locale
+  }), _react["default"].createElement("meta", {
+    property: "og:url",
+    content: url
+  }), _react["default"].createElement("meta", {
+    property: "og:type",
+    content: type
+  }), _react["default"].createElement("meta", {
+    property: "og:title",
+    content: title
+  }), _react["default"].createElement("meta", {
+    property: "og:description",
+    content: desc
+  }), _react["default"].createElement("meta", {
+    property: "og:image",
+    content: image
+  }), _react["default"].createElement("meta", {
+    property: "og:image:alt",
+    content: desc
+  }));
+};
 
-var _propTypes = _interopRequireDefault(require("prop-types"));
+Facebook.propTypes = {
+  url: _propTypes["default"].string.isRequired,
+  locale: _propTypes["default"].string.isRequired,
+  type: _propTypes["default"].string,
+  title: _propTypes["default"].string.isRequired,
+  desc: _propTypes["default"].string.isRequired,
+  image: _propTypes["default"].string.isRequired,
+  name: _propTypes["default"].string
+};
+Facebook.defaultProps = {
+  type: 'website',
+  name: null
+};
 
-var _react = _interopRequireDefault(require("react"));
-
-var _reactHelmet = _interopRequireDefault(require("react-helmet"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-var Twitter = function Twitter(_ref) {
-  var type = _ref.type,
-      username = _ref.username,
-      title = _ref.title,
-      desc = _ref.desc,
-      image = _ref.image;
+var Twitter = function Twitter(_ref3) {
+  var type = _ref3.type,
+      username = _ref3.username,
+      title = _ref3.title,
+      desc = _ref3.desc,
+      image = _ref3.image;
   return _react["default"].createElement(_reactHelmet["default"], null, username && _react["default"].createElement("meta", {
     name: "twitter:creator",
     content: username
@@ -189,7 +129,6 @@ var Twitter = function Twitter(_ref) {
   }));
 };
 
-exports.Twitter = Twitter;
 Twitter.propTypes = {
   type: _propTypes["default"].string,
   username: _propTypes["default"].string,
--- src/index.js
+++ src/index.js
@@ -1,7 +1,102 @@
-import { Facebook } from './facebook'
-import { SEO } from './seo'
-import { Twitter } from './twitter'
+import PropTypes from 'prop-types'
+import React from 'react'
+import Helmet from 'react-helmet'
 
-export { Facebook, Twitter }
+export const SEO = ({
+  title,
+  description,
+  pathname,
+  article,
+  image,
+}) => {
+  const imagePath = `${siteUrl}${
+    image.publicURL ? 'undefined' : defaultImage
+  }`
 
-export default SEO
+  const seo = {
+    title: title || defaultTitle,
+    description: description || defaultDescription,
+    url: `${siteUrl}${pathname || '/'}`,
+    image: `${imagePath || defaultImage}`,
+  }
+
+  return (
+    <>
+      <Helmet title={seo.title}>
+        <html lang={siteLanguage} />
+        <link rel="canonical" href={seo.url} />
+        <meta name="description" content={seo.description} />
+      </Helmet>
+      <Facebook
+        desc={seo.description}
+        image={seo.image}
+        title={seo.title}
+        type={article ? 'article' : 'website'}
+        url={seo.url}
+        locale={siteLocale}
+      />
+      <Twitter
+        title={seo.title}
+        image={seo.image}
+        desc={seo.description}
+        username={twitterUsername}
+      />
+    </>
+  )
+}
+
+SEO.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string,
+}
+
+const Facebook = ({ url, type, title, desc, image, locale }) => (
+  <Helmet>
+    <meta property="og:locale" content={locale} />
+    <meta property="og:url" content={url} />
+    <meta property="og:type" content={type} />
+    <meta property="og:title" content={title} />
+    <meta property="og:description" content={desc} />
+    <meta property="og:image" content={image} />
+    <meta property="og:image:alt" content={desc} />
+  </Helmet>
+)
+
+Facebook.propTypes = {
+  url: PropTypes.string.isRequired,
+  locale: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  desc: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  name: PropTypes.string,
+}
+
+Facebook.defaultProps = {
+  type: 'website',
+  name: null,
+}
+
+const Twitter = ({ type, username, title, desc, image }) => (
+  <Helmet>
+    {username && <meta name="twitter:creator" content={username} />}
+    <meta name="twitter:card" content={type} />
+    <meta name="twitter:title" content={title} />
+    <meta name="twitter:description" content={desc} />
+    <meta name="twitter:image" content={image} />
+    <meta name="twitter:image:alt" content={desc} />
+  </Helmet>
+)
+
+Twitter.propTypes = {
+  type: PropTypes.string,
+  username: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  desc: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+}
+
+Twitter.defaultProps = {
+  type: 'summary_large_image',
+  username: null,
+}
