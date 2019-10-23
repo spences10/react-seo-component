93cc543125b9065722dbcc77b4a7c9fc9549cbb1
spences10 <spences10apps@gmail.com>
2019-10-23 06:57:55 +0100

    add json ld

--- src/seo.js
+++ src/seo.js
@@ -13,13 +13,139 @@ export const SEO = ({
   siteLanguage,
   siteLocale,
   twitterUsername,
+  author,
+  datePublished,
+  dateModified,
 }) => {
+  // schema.org in JSONLD format
+  // https://developers.google.com/search/docs/guides/intro-structured-data
+  // You can fill out the 'author', 'creator' with more data or another type (e.g. 'Organization')
+
+  const schemaOrgWebPage = {
+    '@context': 'http://schema.org',
+    '@type': 'WebPage',
+    url: pathname,
+    headline: description,
+    inLanguage: siteLanguage,
+    mainEntityOfPage: pathname,
+    description: description,
+    name: title,
+    author: {
+      '@type': 'Person',
+      name: author,
+    },
+    copyrightHolder: {
+      '@type': 'Person',
+      name: author,
+    },
+    copyrightYear: '2019',
+    creator: {
+      '@type': 'Person',
+      name: author,
+    },
+    publisher: {
+      '@type': 'Person',
+      name: author,
+    },
+    datePublished,
+    dateModified,
+    image: {
+      '@type': 'ImageObject',
+      url: `${image}`,
+    },
+  }
+
+  // Initial breadcrumb list
+
+  const itemListElement = [
+    {
+      '@type': 'ListItem',
+      item: {
+        '@id': pathname,
+        name: 'Homepage',
+      },
+      position: 1,
+    },
+  ]
+
+  let schemaArticle = null
+
+  if (article) {
+    schemaArticle = {
+      '@context': 'http://schema.org',
+      '@type': 'Article',
+      author: {
+        '@type': 'Person',
+        name: author,
+      },
+      copyrightHolder: {
+        '@type': 'Person',
+        name: author,
+      },
+      copyrightYear: '2019',
+      creator: {
+        '@type': 'Person',
+        name: author,
+      },
+      publisher: {
+        '@type': 'Organization',
+        name: author,
+        logo: {
+          '@type': 'ImageObject',
+          url: `${image}`,
+        },
+      },
+      datePublished,
+      dateModified,
+      description,
+      headline: title,
+      inLanguage: siteLanguage,
+      url,
+      name: title,
+      image: {
+        '@type': 'ImageObject',
+        url: image,
+      },
+      mainEntityOfPage: url,
+    }
+    // Push current blogpost into breadcrumb list
+    itemListElement.push({
+      '@type': 'ListItem',
+      item: {
+        '@id': url,
+        name: title,
+      },
+      position: 2,
+    })
+  }
+
+  const breadcrumb = {
+    '@context': 'http://schema.org',
+    '@type': 'BreadcrumbList',
+    description: 'Breadcrumbs list',
+    name: 'Breadcrumbs',
+    itemListElement,
+  }
+
   return (
     <>
       <Helmet title={title}>
         <html lang={siteLanguage ? siteLanguage : 'en'} />
         <link rel="canonical" href={pathname} />
         <meta name="description" content={description} />
+        {!article && (
+          <script type="application/ld+json">
+            {JSON.stringify(schemaOrgWebPage)}
+          </script>
+        )}
+        {article && (
+          <script type="application/ld+json">
+            {JSON.stringify(schemaArticle)}
+          </script>
+        )}
+        <script type="application/ld+json">
+          {JSON.stringify(breadcrumb)}
+        </script>
       </Helmet>
       <Facebook
         desc={description}
