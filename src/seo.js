d625b1fff1d73a586b810e12415626f7b3d0ce8b
spences10 <spences10apps@gmail.com>
2019-10-23 06:11:49 +0100

    revers to module

---
+++ src/seo.js
@@ -0,0 +1,45 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import Helmet from 'react-helmet'
+import { Facebook } from './facebook'
+import { Twitter } from './twitter'
+
+export const SEO = ({
+  title,
+  description,
+  pathname,
+  article,
+  image,
+  siteLanguage,
+  siteLocale,
+  twitterUsername,
+}) => {
+  return (
+    <>
+      <Helmet title={title}>
+        <html lang={siteLanguage ? siteLanguage : 'en'} />
+        <link rel="canonical" href={pathname} />
+        <meta name="description" content={description} />
+      </Helmet>
+      <Facebook
+        desc={description}
+        image={image}
+        title={title}
+        type={article ? 'article' : 'website'}
+        url={pathname}
+        locale={siteLocale ? siteLocale : 'en_gb'}
+      />
+      <Twitter
+        title={title}
+        image={image}
+        desc={description}
+        username={twitterUsername}
+      />
+    </>
+  )
+}
+
+SEO.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string,
+}
