ab744a67041caf4eb4c6ebafa07c4930610db4f9
spences10 <spences10apps@gmail.com>
2020-04-11 15:48:19 +0100

    switch to TS from JS

---
+++ src/twitter.tsx
@@ -0,0 +1,27 @@
+import React from "react"
+import { Helmet } from "react-helmet-async"
+
+interface Props {
+  type?: string // optional denoted with ?
+  username: string
+  title: string
+  desc: string
+  image: string
+}
+
+export const Twitter = ({
+  type = "summary_large_image",
+  username,
+  title,
+  desc,
+  image,
+}: Props) => (
+  <Helmet>
+    {username && <meta name="twitter:creator" content={username} />}
+    <meta name="twitter:card" content={type} />
+    <meta name="twitter:title" content={title} />
+    <meta name="twitter:description" content={desc} />
+    <meta name="twitter:image" content={image} />
+    <meta name="twitter:image:alt" content={desc} />
+  </Helmet>
+)
